eaa49e643e49765215d05946cf9c2b2ef6c2f890
DebaR200 <h.22.debashis@gmail.com>
2025-08-12 11:06:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-div{
-	width:200px;
-	height:200px;
-	border:2px solid black;
-	color:light blue;
+body {
+    height: 100vh; /* Make sure the body takes the full height of the viewport */
+    display: flex;
+    justify-content: center; /* Center horizontally */
+    align-items: center; /* Center vertically */
+    margin: 0; /* Remove default margin */
+}
+
+.box {
+    width: 200px;
+    height: 200px;
+    border: 2px solid black;
+    background-color: lightblue;
+    display: flex; /* Use flexbox to center the text */
+    justify-content: center; /* Center text horizontally */
+    align-items: center; /* Center text vertically */
+}
+
+.box p {
+    color: purple;
+    margin: 0; /* Remove default margin from paragraph */
 }
